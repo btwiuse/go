6422d573b210b65692973e54a30c6bc95c8b3d75
Austin Clements <austin@google.com>
2026-08-27 10:49:56 -0400

    simd/archsimd/_gen/simdgen: refactor operand sorting
    This moves operand comparison to a method of Operand to decouple it
    from Operation while making it easily sharable.
    
    Change-Id: Ifc4f5ca88b8fe7219babe042cf332ca9a8f39657
    Reviewed-on: https://go-review.googlesource.com/c/go/+/823224
    Reviewed-by: David Chase <drchase@google.com>
    Auto-Submit: Austin Clements <austin@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- src/simd/archsimd/_gen/simdgen/gen_utility.go
+++ src/simd/archsimd/_gen/simdgen/gen_utility.go
@@ -15,7 +15,6 @@ import (
 	"reflect"
 	"simd/archsimd/_gen/simdgen/types"
 	"slices"
-	"sort"
 	"strings"
 	"text/template" // NOLINT
 	"unicode"
@@ -390,15 +389,7 @@ func (op *Operation) regShape(mem memShape) (string, error) {
 // from my observation looks like in asm, imms are always the first,
 // masks are always the last, with vreg in between.
 func (op *Operation) sortOperand() {
-	priority := map[string]int{"immediate": 0, "vreg": 1, "greg": 1, "mask": 2}
-	sort.SliceStable(op.In, func(i, j int) bool {
-		pi := priority[op.In[i].Class]
-		pj := priority[op.In[j].Class]
-		if pi != pj {
-			return pi < pj
-		}
-		return op.In[i].AsmPos < op.In[j].AsmPos
-	})
+	slices.SortStableFunc(op.In, types.Operand.Compare)
 }
 
 // adjustAsm adjusts the asm to make it align with Go's assembler.
