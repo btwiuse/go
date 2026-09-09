btwiuse/go

commit d87b733482eeb1509572611244331f944c7f1608

go/types, types2: allow target to hold any type, not just signatures
target is used for function type inference in assignment context.
When creating a new target, newTarget returns a non-nil result
only if the provided target type is a signature.

Adjust target and newTarget to hold any (non-nil) type and provide
a sig accessor method to see if the target is in fact a signature.

This delays the signature computation to the point of use (rare)
rather than the point of target creation (each assignment).

Preparation for wider use of target (forthcoming CLs) with the goal
to remove the other hint types.

Change-Id: I2a118107975453c89c1d4adbecfc32ab0730b285
Reviewed-on: https://go-review.googlesource.com/c/go/+/818840
Reviewed-by: Robert Griesemer <gri@google.com>
LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Auto-Submit: Robert Griesemer <gri@google.com>
Reviewed-by: Mark Freeman <mark@golang.org>

src/cmd/compile/internal/types2/call.go
@@ -31,7 +31,8 @@ import (
 // If an error (other than a version error) occurs in any case, it is reported
 // and x.mode is set to invalid.
 func (check *Checker) funcInst(T *target, pos syntax.Pos, x *operand, inst *syntax.IndexExpr, infer bool) []Type {
-	assert(T != nil || inst != nil)
+	Tsig := T.sig()
+	assert(Tsig != nil || inst != nil)
 
 	var instErrPos poser
 	if inst != nil {
@@ -99,7 +100,7 @@ func (check *Checker) funcInst(T *target, pos syntax.Pos, x *operand, inst *synt
 			// or the result type in a return statement. Create a pseudo-expression for that operand
 			// that makes sense when reported in error messages from infer, below.
 			expr := syntax.NewName(x.Pos(), T.desc)
-			args = []*operand{{mode_: value, expr: expr, typ_: T.sig}}
+			args = []*operand{{mode_: value, expr: expr, typ_: Tsig}}
 			reverse = true
 		}
 

src/go/types/call.go
@@ -32,7 +32,8 @@ import (
 // If an error (other than a version error) occurs in any case, it is reported
 // and x.mode is set to invalid.
 func (check *Checker) funcInst(T *target, pos token.Pos, x *operand, ix *indexedExpr, infer bool) []Type {
-	assert(T != nil || ix != nil)
+	Tsig := T.sig()
+	assert(Tsig != nil || ix != nil)
 
 	var instErrPos positioner
 	if ix != nil {
@@ -101,7 +102,7 @@ func (check *Checker) funcInst(T *target, pos token.Pos, x *operand, ix *indexed
 			// that makes sense when reported in error messages from infer, below.
 			expr := ast.NewIdent(T.desc)
 			expr.NamePos = x.Pos() // correct position
-			args = []*operand{{mode_: value, expr: expr, typ_: T.sig}}
+			args = []*operand{{mode_: value, expr: expr, typ_: Tsig}}
 			reverse = true
 		}
 

src/cmd/compile/internal/types2/expr.go
@@ -953,21 +953,30 @@ const (
 	statement
 )
 
-// target represent the (signature) type and description of the LHS
+// target represent the type and description of the LHS
 // variable of an assignment, or of a function result variable.
 type target struct {
-	sig  *Signature
+	typ  Type
 	desc string
 }
 
 // newTarget creates a new target for the given type and description.
-// The result is nil if typ is not a signature.
+// The result is nil if typ is nil.
 func newTarget(typ Type, desc string) *target {
 	if typ != nil {
-		if u, _ := commonUnder(typ, nil); u != nil {
-			if sig, _ := u.(*Signature); sig != nil {
-				return &target{sig, desc}
-			}
+		return &target{typ, desc}
+	}
+	return nil
+}
+
+// sig returns the target type T as a signature if it exists.
+// The result is nil if T is nil or T's type set has no common
+// signature type.
+func (T *target) sig() *Signature {
+	if T != nil {
+		if u, _ := commonUnder(T.typ, nil); u != nil {
+			sig, _ := u.(*Signature)
+			return sig // possibly nil
 		}
 	}
 	return nil
@@ -1018,7 +1027,7 @@ func (check *Checker) nonGeneric(T *target, x *operand) {
 		}
 	case *Signature:
 		if t.tparams != nil {
-			if enableReverseTypeInference && T != nil {
+			if enableReverseTypeInference && T.sig() != nil {
 				check.funcInst(T, x.Pos(), x, nil, true)
 				return
 			}

src/go/types/expr.go
@@ -945,21 +945,30 @@ const (
 	statement
 )
 
-// target represent the (signature) type and description of the LHS
+// target represent the type and description of the LHS
 // variable of an assignment, or of a function result variable.
 type target struct {
-	sig  *Signature
+	typ  Type
 	desc string
 }
 
 // newTarget creates a new target for the given type and description.
-// The result is nil if typ is not a signature.
+// The result is nil if typ is nil.
 func newTarget(typ Type, desc string) *target {
 	if typ != nil {
-		if u, _ := commonUnder(typ, nil); u != nil {
-			if sig, _ := u.(*Signature); sig != nil {
-				return &target{sig, desc}
-			}
+		return &target{typ, desc}
+	}
+	return nil
+}
+
+// sig returns the target type T as a signature if it exists.
+// The result is nil if T is nil or T's type set has no common
+// signature type.
+func (T *target) sig() *Signature {
+	if T != nil {
+		if u, _ := commonUnder(T.typ, nil); u != nil {
+			sig, _ := u.(*Signature)
+			return sig // possibly nil
 		}
 	}
 	return nil
@@ -1010,7 +1019,7 @@ func (check *Checker) nonGeneric(T *target, x *operand) {
 		}
 	case *Signature:
 		if t.tparams != nil {
-			if enableReverseTypeInference && T != nil {
+			if enableReverseTypeInference && T.sig() != nil {
 				check.funcInst(T, x.Pos(), x, nil, true)
 				return
 			}

commit 6939463d82cdf9701036d3861ecb2753dac33773

go/types, types2: do nil check on correct variable Tsig, not T
This fixes a regression introduced by CL 818840. That CL changed
the meaning of a target. Before the change, target was nil if it
was unknown or not representing a *Signature.

Now, target is nil if it is unknown. It is non-nil otherwise, even
if it is not representing a *Signature. Now, target.sig() != nil
is the respective test.

The change was done correctly in call.go on line 36 (35 for types2),
but missed on line 90 (89). Fix the oversight.

Fixes #81397.

Change-Id: Iea5b673678a882b2df87f760e7726cf867b79d92
Reviewed-on: https://go-review.googlesource.com/c/go/+/829544
Reviewed-by: Robert Griesemer <gri@google.com>
Auto-Submit: Robert Griesemer <gri@google.com>
Reviewed-by: Mark Freeman <mark@golang.org>
LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

src/cmd/compile/internal/types2/call.go
@@ -86,7 +86,7 @@ func (check *Checker) funcInst(T *target, pos syntax.Pos, x *operand, inst *synt
 		var args []*operand
 		var params []*Var
 		var reverse bool
-		if T != nil && sig.tparams != nil {
+		if Tsig != nil && sig.tparams != nil {
 			if !versionErr && !check.allowVersion(go1_21) {
 				if inst != nil {
 					check.versionErrorf(instErrPos, go1_21, "partially instantiated function in assignment")

src/go/types/call.go
@@ -87,7 +87,7 @@ func (check *Checker) funcInst(T *target, pos token.Pos, x *operand, ix *indexed
 		var args []*operand
 		var params []*Var
 		var reverse bool
-		if T != nil && sig.tparams != nil {
+		if Tsig != nil && sig.tparams != nil {
 			if !versionErr && !check.allowVersion(go1_21) {
 				if ix != nil {
 					check.versionErrorf(instErrPos, go1_21, "partially instantiated function in assignment")

src/internal/types/testdata/fixedbugs/issue81397.go
@@ -1,0 +1,10 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package p
+
+func f[P, Q any]() {}
+
+// Don't panic on the next line, produce a proper error instead.
+var _ any = f /* ERROR "cannot infer Q" */ [int]